c956b39633c93033369cf09cabb6ce494edd79e5
TIBO <thibault.marie.pro@gmail.com>
2024-04-05 00:33:00 +0200

    project(ABANDON): project reset, c ya

---
+++ modules/palette.js
@@ -0,0 +1,28 @@
+import {
+    hexToHSL,
+    copyhexCode,
+    displayHexCode,
+    rgbToHex,
+    filterRGBColor,
+    splitComa,
+    createGrid,
+} from "./utils.js";
+import kwulers from 'kwulers';
+
+export default class Palette {
+    constructor(name) {
+        this.name = name.replace(/[^a-zA-Z0-9]/g,'-');
+    }
+    cretaePalette() {
+
+    }
+
+    generatePalette() {
+
+    }
+
+    removePalette() {
+
+    }
+
+}
